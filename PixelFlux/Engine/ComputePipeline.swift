d7eab93f18eb843a5831dc3efd5fb0fbdd60b756
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-11-14 10:02:44 +0300

    implement ComputePipeline and GPUContext classes; add brightness kernel in Brightness.metal

--- PixelFlux/Engine/ComputePipeline.swift
+++ PixelFlux/Engine/ComputePipeline.swift
@@ -1,7 +1,39 @@
-//
-//  ComputePipeline.swift
-//  PixelFlux
-//
-//  Created by omar on 14/11/2025.
-//
+import Foundation
+import Metal
 
+
+final class ComputePipeline {
+let state: MTLComputePipelineState
+private let device: MTLDevice
+
+
+init(functionName: String) throws {
+self.device = GPUContext.shared.device
+guard let lib = device.makeDefaultLibrary() else {
+throw NSError(domain: "PixelFlux", code: -1, userInfo: [NSLocalizedDescriptionKey: "Default Metal library not found. Make sure .metal files are in target."])
+}
+guard let fn = lib.makeFunction(name: functionName) else {
+throw NSError(domain: "PixelFlux", code: -2, userInfo: [NSLocalizedDescriptionKey: "Function \(functionName) not found in default library."])
+}
+self.state = try device.makeComputePipelineState(function: fn)
+}
+
+
+func encode(commandBuffer: MTLCommandBuffer, input: MTLTexture, output: MTLTexture, buffers: [MTLBuffer] = []) {
+guard let encoder = commandBuffer.makeComputeCommandEncoder() else { return }
+encoder.setComputePipelineState(state)
+encoder.setTexture(input, index: 0)
+encoder.setTexture(output, index: 1)
+
+
+for (i, b) in buffers.enumerated() {
+encoder.setBuffer(b, offset: 0, index: i)
+}
+
+
+let threadsPerThreadgroup = MTLSize(width: 16, height: 16, depth: 1)
+let threadsPerGrid = MTLSize(width: input.width, height: input.height, depth: 1)
+encoder.dispatchThreads(threadsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
+encoder.endEncoding()
+}
+}
--- PixelFlux/Engine/GPUContext.swift
+++ PixelFlux/Engine/GPUContext.swift
@@ -1,7 +1,31 @@
-//
-//  GPUContext.swift
-//  PixelFlux
-//
-//  Created by omar on 14/11/2025.
-//
+import Foundation
+import Metal
+import MetalKit
+import CoreImage
 
+
+final class GPUContext {
+static let shared: GPUContext = {
+return try! GPUContext()
+}()
+
+
+let device: MTLDevice
+let queue: MTLCommandQueue
+let ciContext: CIContext
+let textureLoader: MTKTextureLoader
+
+
+private init() throws {
+guard let device = MTLCreateSystemDefaultDevice() else {
+fatalError("Metal not supported on this device")
+}
+self.device = device
+guard let q = device.makeCommandQueue() else {
+fatalError("Failed to create command queue")
+}
+self.queue = q
+self.ciContext = CIContext(mtlDevice: device)
+self.textureLoader = MTKTextureLoader(device: device)
+}
+}
