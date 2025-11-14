d4ef5315bf545209103a62e3e853ca97058e8a71
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2025-11-14 20:13:10 +0300

    initialize PixelFluxDemo project structure; add AppDelegate, SceneDelegate, ViewController, and asset files

---
+++ PixelFluxDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PixelFluxDemo
+//
+//  Created by omar on 14/11/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PixelFluxDemoTests/PixelFluxDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PixelFluxDemoTests.swift
+//  PixelFluxDemoTests
+//
+//  Created by omar on 14/11/2025.
+//
+
+import Testing
+@testable import PixelFluxDemo
+
+struct PixelFluxDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
